a0bdbfabbbbb99a2491d81536eb92546568715b5
Peter Cunha <petercunha8@gmail.com>
2018-10-20 02:21:08 -0400

    Algolia search is working woohoooooooo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Highlight,
+  RefinementList,
+  Pagination,
+  CurrentRefinements,
+  ClearRefinements
+} from 'react-instantsearch-dom';
 import "./App.css";
 
 class Main extends Component {
@@ -31,30 +41,46 @@ class Main extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleUploadImage}>
-        <div>
-          <input name="img" type="file" />
-        </div>
-        <div>
-          <input
-            name="fileName"
-            type="text"
-            placeholder="Enter the desired name of file"
-          />
-        </div>
-        <br />
-        <div>
-          <button>Upload</button>
-        </div>
-        <img src={this.state.imageURL} alt="img" />
-        {this.state.evaluation.map(e => (
-          <p>
-            {e.name}: <i>{e.confidence}</i>
-          </p>
-        ))}
-      </form>
+      <>
+        <form onSubmit={this.handleUploadImage}>
+          <div>
+            <input name="img" type="file" />
+          </div>
+          <div>
+            <input
+              name="fileName"
+              type="text"
+              placeholder="Enter the desired name of file"
+            />
+          </div>
+          <br />
+          <div>
+            <button>Upload</button>
+          </div>
+          <img src={this.state.imageURL} alt="img" />
+          {this.state.evaluation.map(e => (
+            <p>
+              {e.name}: <i>{e.confidence}</i>
+            </p>
+          ))}
+        </form>
+        <InstantSearch
+          appId="EQJPKC2WRW"
+          apiKey="5b04b1e3177d8269cf7c95e282d3b81b"
+          indexName="items"
+        >
+          <SearchBox />
+          <Hits hitComponent={Product} />
+        </InstantSearch>
+      </>
     );
   }
 }
+
+
+function Product({ hit }) {
+  return <img src={hit.image} alt={hit.title} />;
+}
+
 
 export default Main;
